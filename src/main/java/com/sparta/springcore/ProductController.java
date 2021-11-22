167b000ee25f4297e468241fad107a014b229766
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2021-11-22 23:56:57 +0900

    first try

--- src/main/java/com/sparta/springcore/ProductController.java
+++ src/main/java/com/sparta/springcore/ProductController.java
@@ -1,4 +1,34 @@
 package com.sparta.springcore;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class ProductController {
+
+    @PostMapping("/api/products")
+    public Product createProduct(@RequestBody ProductRequestDto requestDto) throws SQLException {
+        ProductService productService = new ProductService();
+        Product product = productService.createProduct(requestDto);
+        return product;
+    }
+
+    @PutMapping("/api/products/{id}")
+    public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) throws SQLException {
+        Product product = new Product();
+
+        return product.getId();
+    }
+
+    @GetMapping("/api/products")
+    public List<Product> getProducts() throws SQLException {
+        ProductService productService = new ProductService();
+        List<Product> products = productService.getProducts();
+        return products;
+    }
 }
--- src/main/java/com/sparta/springcore/ProductService.java
+++ src/main/java/com/sparta/springcore/ProductService.java
@@ -1,4 +1,34 @@
 package com.sparta.springcore;
 
+import java.sql.SQLException;
+import java.util.List;
+
 public class ProductService {
+    public Product createProduct(ProductRequestDto requestDto) throws SQLException {
+    Product product = new Product(requestDto);
+
+    ProductRepository productRepository = new ProductRepository();
+    productRepository.createProduct(product);
+    return product;
+    }
+
+    public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) throws SQLException {
+        ProductRepository productRepository = new ProductRepository();
+        Product product = productRepository.getProduct(id);
+        if (requestDto.getMyprice() <=0) {
+            throw new RuntimeException("최저가를 0 이상으로 입력하십시오.");
+        }
+
+        int myprice = requestDto.getMyprice();
+        productRepository.updateMyprice(id, myprice);
+        return product;
+    }
+
+    public List<Product> getProducts() throws SQLException {
+        ProductRepository productRepository = new ProductRepository();
+        List<Product> products = productRepository.getProducts();
+
+        return products;
+    }
+
 }
