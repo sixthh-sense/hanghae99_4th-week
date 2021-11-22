18b2864177950b64228bae4494037d3fb2639466
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2021-11-23 02:56:48 +0900

    ItemSearch_refactor

--- src/main/java/com/sparta/springcore/controller/ItemSearchController.java
+++ src/main/java/com/sparta/springcore/controller/ItemSearchController.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.springcore.dto.ItemDto;
+import com.sparta.springcore.service.ItemSearchService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -21,6 +23,13 @@ import java.util.List;
 @Controller
 public class ItemSearchController {
 
+    private final ItemSearchService itemSearchService;
+
+    @Autowired
+    public ItemSearchController(ItemSearchService itemSearchService) {
+        this.itemSearchService = itemSearchService;
+    }
+
     // Controller 가 자동으로 해주는 일
 // 1. API Request 의 파라미터 값에서 검색어 추출 -> query 변수
 // 5. API Response 보내기
@@ -29,27 +38,8 @@ public class ItemSearchController {
     @GetMapping("/api/search")
     @ResponseBody
     public List<ItemDto> getItems(@RequestParam String query) throws IOException {
-// 2. 네이버 쇼핑 API 호출에 필요한 Header, Body 정리
-        RestTemplate rest = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("X-Naver-Client-Id", "YhGDVSRN8cGNYdNRJmqY"); // zdqMoIkFaK8uKvC2oNY2
-        headers.add("X-Naver-Client-Secret", "TBGQ5JUiT4"); // LiZfsgtuD5
-        String body = "";
-        HttpEntity<String> requestEntity = new HttpEntity<>(body, headers);
 
-// 3. 네이버 쇼핑 API 호출 결과 -> naverApiResponseJson (JSON 형태)
-        ResponseEntity<String> responseEntity = rest.exchange("https://openapi.naver.com/v1/search/shop.json?query=" + query, HttpMethod.GET, requestEntity, String.class);
-        String naverApiResponseJson = responseEntity.getBody();
-
-// 4. naverApiResponseJson (JSON 형태) -> itemDtoList (자바 객체 형태)
-// - naverApiResponseJson 에서 우리가 사용할 데이터만 추출 -> List<ItemDto> 객체로 변환
-        ObjectMapper objectMapper = new ObjectMapper()
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        JsonNode itemsNode = objectMapper.readTree(naverApiResponseJson).get("items");
-        List<ItemDto> itemDtoList = objectMapper
-                .readerFor(new TypeReference<List<ItemDto>>() {})
-                .readValue(itemsNode);
-
+        List<ItemDto> itemDtoList = itemSearchService.searchItems(query);
         return itemDtoList;
     }
 }
